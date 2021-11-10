70783b30901b56a26ab5dc1b88c6755b9ec5c5fc
Antonio Marino <marino.antonio375@gmail.com>
2021-11-09 20:30:53 -0500

    linking sql

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const view = require("./lib/view");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3001,
   user: "root",
-  password: "eumamgreshit5",
+  password: "Antonio",
   database: "company_db"
 });
 
